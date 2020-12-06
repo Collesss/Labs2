c30ae3d53e3beb62314945477e3e577e69a68311
Sergey Lesikov <sergey_lesikov@mail.ru>
2020-12-06 21:04:44 +0300

    update

--- YaP-Kursovaya_rabota-C/FormCourseWork.Designer.cs
+++ YaP-Kursovaya_rabota-C/FormCourseWork.Designer.cs
@@ -29,76 +29,275 @@ namespace YaP_Kursovaya_rabota_C
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox1Input = new System.Windows.Forms.ListBox();
+            this.textBox1Num = new System.Windows.Forms.TextBox();
+            this.label4Num = new System.Windows.Forms.Label();
+            this.button2Calculate = new System.Windows.Forms.Button();
+            this.listBox2Output = new System.Windows.Forms.ListBox();
+            this.button3ClearList = new System.Windows.Forms.Button();
+            this.button4Exit = new System.Windows.Forms.Button();
+            this.label7Avg = new System.Windows.Forms.Label();
+            this.textBox2Avg = new System.Windows.Forms.TextBox();
+            this.label8Count = new System.Windows.Forms.Label();
+            this.textBox3Count = new System.Windows.Forms.TextBox();
+            this.button1Add = new System.Windows.Forms.Button();
+            this.label1TextBlue = new System.Windows.Forms.Label();
+            this.label2TextBlue = new System.Windows.Forms.Label();
+            this.label3Text = new System.Windows.Forms.Label();
+            this.label5InputArray = new System.Windows.Forms.Label();
+            this.label6OutputArray = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // listBox1Input
             // 
-            this.listBox1.ColumnWidth = 50;
-            this.listBox1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.ItemHeight = 17;
-            this.listBox1.Items.AddRange(new object[] {
-            "412",
-            "421",
-            "4",
-            "124",
-            "5351",
-            "136",
-            "1236",
-            "246",
-            "234636",
-            "347",
-            "37",
-            "125",
-            "1",
-            "25",
-            "165",
-            "2",
-            "15",
-            "521512",
-            "6512",
-            "5",
-            "15245624626236",
-            "263236",
-            "26236263236",
-            "236",
-            "623263",
-            "1235",
-            "25",
-            "261",
-            "61",
-            "6",
-            "1",
-            "36",
-            "13",
-            "61",
-            "36",
-            "1",
-            "36"});
-            this.listBox1.Location = new System.Drawing.Point(164, 248);
-            this.listBox1.MultiColumn = true;
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(454, 38);
-            this.listBox1.TabIndex = 0;
+            this.listBox1Input.ColumnWidth = 50;
+            this.listBox1Input.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1Input.FormattingEnabled = true;
+            this.listBox1Input.HorizontalScrollbar = true;
+            this.listBox1Input.ItemHeight = 17;
+            this.listBox1Input.Location = new System.Drawing.Point(210, 220);
+            this.listBox1Input.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1Input.MultiColumn = true;
+            this.listBox1Input.Name = "listBox1Input";
+            this.listBox1Input.ScrollAlwaysVisible = true;
+            this.listBox1Input.Size = new System.Drawing.Size(600, 38);
+            this.listBox1Input.TabIndex = 0;
+            // 
+            // textBox1Num
+            // 
+            this.textBox1Num.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1Num.Location = new System.Drawing.Point(290, 160);
+            this.textBox1Num.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1Num.Name = "textBox1Num";
+            this.textBox1Num.Size = new System.Drawing.Size(155, 24);
+            this.textBox1Num.TabIndex = 1;
+            // 
+            // label4Num
+            // 
+            this.label4Num.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4Num.Location = new System.Drawing.Point(50, 160);
+            this.label4Num.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4Num.Name = "label4Num";
+            this.label4Num.Size = new System.Drawing.Size(235, 24);
+            this.label4Num.TabIndex = 2;
+            this.label4Num.Text = "Число для добавления в список";
+            this.label4Num.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button2Calculate
+            // 
+            this.button2Calculate.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2Calculate.Location = new System.Drawing.Point(560, 147);
+            this.button2Calculate.Margin = new System.Windows.Forms.Padding(4);
+            this.button2Calculate.Name = "button2Calculate";
+            this.button2Calculate.Size = new System.Drawing.Size(100, 50);
+            this.button2Calculate.TabIndex = 3;
+            this.button2Calculate.Text = "Вычислить";
+            this.button2Calculate.UseVisualStyleBackColor = true;
+            this.button2Calculate.Click += new System.EventHandler(this.button1Calculate_Click);
+            // 
+            // listBox2Output
+            // 
+            this.listBox2Output.ColumnWidth = 50;
+            this.listBox2Output.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2Output.FormattingEnabled = true;
+            this.listBox2Output.HorizontalScrollbar = true;
+            this.listBox2Output.ItemHeight = 17;
+            this.listBox2Output.Location = new System.Drawing.Point(210, 268);
+            this.listBox2Output.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2Output.MultiColumn = true;
+            this.listBox2Output.Name = "listBox2Output";
+            this.listBox2Output.ScrollAlwaysVisible = true;
+            this.listBox2Output.Size = new System.Drawing.Size(600, 38);
+            this.listBox2Output.TabIndex = 4;
+            // 
+            // button3ClearList
+            // 
+            this.button3ClearList.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3ClearList.Location = new System.Drawing.Point(665, 147);
+            this.button3ClearList.Margin = new System.Windows.Forms.Padding(4);
+            this.button3ClearList.Name = "button3ClearList";
+            this.button3ClearList.Size = new System.Drawing.Size(100, 50);
+            this.button3ClearList.TabIndex = 5;
+            this.button3ClearList.Text = "Очистить список";
+            this.button3ClearList.UseVisualStyleBackColor = true;
+            this.button3ClearList.Click += new System.EventHandler(this.button2ClearList_Click);
+            // 
+            // button4Exit
+            // 
+            this.button4Exit.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4Exit.Location = new System.Drawing.Point(770, 147);
+            this.button4Exit.Margin = new System.Windows.Forms.Padding(4);
+            this.button4Exit.Name = "button4Exit";
+            this.button4Exit.Size = new System.Drawing.Size(100, 50);
+            this.button4Exit.TabIndex = 6;
+            this.button4Exit.Text = "Выход";
+            this.button4Exit.UseVisualStyleBackColor = true;
+            this.button4Exit.Click += new System.EventHandler(this.button3Exit_Click);
+            // 
+            // label7Avg
+            // 
+            this.label7Avg.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7Avg.Location = new System.Drawing.Point(10, 330);
+            this.label7Avg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7Avg.Name = "label7Avg";
+            this.label7Avg.Size = new System.Drawing.Size(270, 24);
+            this.label7Avg.TabIndex = 8;
+            this.label7Avg.Text = "Средее арифмитическое массива";
+            this.label7Avg.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2Avg
+            // 
+            this.textBox2Avg.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2Avg.Location = new System.Drawing.Point(285, 330);
+            this.textBox2Avg.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2Avg.Name = "textBox2Avg";
+            this.textBox2Avg.ReadOnly = true;
+            this.textBox2Avg.Size = new System.Drawing.Size(155, 24);
+            this.textBox2Avg.TabIndex = 7;
+            // 
+            // label8Count
+            // 
+            this.label8Count.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8Count.Location = new System.Drawing.Point(450, 330);
+            this.label8Count.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8Count.Name = "label8Count";
+            this.label8Count.Size = new System.Drawing.Size(300, 24);
+            this.label8Count.TabIndex = 10;
+            this.label8Count.Text = "Количество элементов в новом массиве";
+            this.label8Count.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox3Count
+            // 
+            this.textBox3Count.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3Count.Location = new System.Drawing.Point(755, 330);
+            this.textBox3Count.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3Count.Name = "textBox3Count";
+            this.textBox3Count.ReadOnly = true;
+            this.textBox3Count.Size = new System.Drawing.Size(155, 24);
+            this.textBox3Count.TabIndex = 9;
+            // 
+            // button1Add
+            // 
+            this.button1Add.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1Add.Location = new System.Drawing.Point(455, 147);
+            this.button1Add.Margin = new System.Windows.Forms.Padding(4);
+            this.button1Add.Name = "button1Add";
+            this.button1Add.Size = new System.Drawing.Size(100, 50);
+            this.button1Add.TabIndex = 11;
+            this.button1Add.Text = "Добавить в список";
+            this.button1Add.UseVisualStyleBackColor = true;
+            this.button1Add.Click += new System.EventHandler(this.button1Add_Click);
+            // 
+            // label1TextBlue
+            // 
+            this.label1TextBlue.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1TextBlue.ForeColor = System.Drawing.Color.Blue;
+            this.label1TextBlue.Location = new System.Drawing.Point(285, 15);
+            this.label1TextBlue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1TextBlue.Name = "label1TextBlue";
+            this.label1TextBlue.Size = new System.Drawing.Size(350, 23);
+            this.label1TextBlue.TabIndex = 12;
+            this.label1TextBlue.Text = "Изучение одномерных массивов";
+            this.label1TextBlue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2TextBlue
+            // 
+            this.label2TextBlue.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2TextBlue.ForeColor = System.Drawing.Color.Blue;
+            this.label2TextBlue.Location = new System.Drawing.Point(70, 70);
+            this.label2TextBlue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2TextBlue.Name = "label2TextBlue";
+            this.label2TextBlue.Size = new System.Drawing.Size(145, 33);
+            this.label2TextBlue.TabIndex = 13;
+            this.label2TextBlue.Text = "Задание:";
+            this.label2TextBlue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3Text
+            // 
+            this.label3Text.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3Text.Location = new System.Drawing.Point(220, 55);
+            this.label3Text.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3Text.Name = "label3Text";
+            this.label3Text.Size = new System.Drawing.Size(630, 60);
+            this.label3Text.TabIndex = 14;
+            this.label3Text.Text = "Ввести одномерный массив ручным вводом\r\nи вывести элементы массива которые меньше" +
+    " среднего арифметического массива\r\nи количество элементов в полученном массиве и" +
+    " среднее арифметическое массива";
+            this.label3Text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5InputArray
+            // 
+            this.label5InputArray.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5InputArray.Location = new System.Drawing.Point(105, 220);
+            this.label5InputArray.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5InputArray.Name = "label5InputArray";
+            this.label5InputArray.Size = new System.Drawing.Size(100, 38);
+            this.label5InputArray.TabIndex = 15;
+            this.label5InputArray.Text = "Исходный\r\nмассив";
+            this.label5InputArray.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6OutputArray
+            // 
+            this.label6OutputArray.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6OutputArray.Location = new System.Drawing.Point(105, 268);
+            this.label6OutputArray.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6OutputArray.Name = "label6OutputArray";
+            this.label6OutputArray.Size = new System.Drawing.Size(100, 38);
+            this.label6OutputArray.TabIndex = 16;
+            this.label6OutputArray.Text = "Полученный\r\nмассив";
+            this.label6OutputArray.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // FormCourseWork
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(924, 366);
+            this.Controls.Add(this.label6OutputArray);
+            this.Controls.Add(this.label5InputArray);
+            this.Controls.Add(this.label3Text);
+            this.Controls.Add(this.label2TextBlue);
+            this.Controls.Add(this.label1TextBlue);
+            this.Controls.Add(this.button1Add);
+            this.Controls.Add(this.label8Count);
+            this.Controls.Add(this.textBox3Count);
+            this.Controls.Add(this.label7Avg);
+            this.Controls.Add(this.textBox2Avg);
+            this.Controls.Add(this.button4Exit);
+            this.Controls.Add(this.button3ClearList);
+            this.Controls.Add(this.listBox2Output);
+            this.Controls.Add(this.button2Calculate);
+            this.Controls.Add(this.label4Num);
+            this.Controls.Add(this.textBox1Num);
+            this.Controls.Add(this.listBox1Input);
+            this.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "FormCourseWork";
             this.Text = "КР «Программирование алгоритмов формирования и обработки одномерных массивов»";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox1Input;
+        private System.Windows.Forms.TextBox textBox1Num;
+        private System.Windows.Forms.Label label4Num;
+        private System.Windows.Forms.Button button2Calculate;
+        private System.Windows.Forms.ListBox listBox2Output;
+        private System.Windows.Forms.Button button3ClearList;
+        private System.Windows.Forms.Button button4Exit;
+        private System.Windows.Forms.Label label7Avg;
+        private System.Windows.Forms.TextBox textBox2Avg;
+        private System.Windows.Forms.Label label8Count;
+        private System.Windows.Forms.TextBox textBox3Count;
+        private System.Windows.Forms.Button button1Add;
+        private System.Windows.Forms.Label label1TextBlue;
+        private System.Windows.Forms.Label label2TextBlue;
+        private System.Windows.Forms.Label label3Text;
+        private System.Windows.Forms.Label label5InputArray;
+        private System.Windows.Forms.Label label6OutputArray;
     }
 }
 
--- YaP-Kursovaya_rabota-C/FormCourseWork.cs
+++ YaP-Kursovaya_rabota-C/FormCourseWork.cs
@@ -17,29 +17,12 @@ namespace YaP_Kursovaya_rabota_C
             InitializeComponent();
         }
 
-
+        //функция расчитывающая среднее арифметическое массива и выводящая элементы массива которые мень меньше среднего арифметического в ListBox
         private void Calculate(double[] nums, out double avg, out int countLessAvg)
         {
+            listBox2Output.Items.Clear();
+
             countLessAvg = 0;
-
-            avg = 0;
-
-            for (int i = 0; i < nums.Length; i++)
-                avg += nums[i];
-
-            avg /= nums.Length;
-
-            for (int i = 0; i < nums.Length; i++)
-                if (nums[i] < avg)
-                    countLessAvg++;
-        }
-
-        private double[] CalculateV2(double[] nums, out double avg)
-        {
-            double[] numsLessAvg = new double[nums.Length];
-
-            int countLessAvg = 0;
-
             avg = 0;
 
             for (int i = 0; i < nums.Length; i++)
@@ -50,16 +33,59 @@ namespace YaP_Kursovaya_rabota_C
             for (int i = 0; i < nums.Length; i++)
                 if (nums[i] < avg)
                 {
-                    numsLessAvg[countLessAvg] = nums[i];
                     countLessAvg++;
+                    listBox2Output.Items.Add(nums[i].ToString("F2"));
                 }
-
-            double[] returnArray = new double[countLessAvg];
-
-            Array.Copy(numsLessAvg, 0, returnArray, 0, countLessAvg);
-
-            return returnArray;
         }
 
+        //функция вызываемая при нажатии на кнопку выхода
+        private void button3Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //функция вызываемая при нажатии на кнопку
+        private void button1Calculate_Click(object sender, EventArgs e)
+        {
+            double[] nums = Input();
+
+            double avg;
+            int count;
+
+            Calculate(nums, out avg, out count);
+
+            Output(count, avg);
+        }
+
+        
+        //функция ввода
+        private double[] Input()
+        {
+            double[] nums = new double[listBox1Input.Items.Count];
+
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = double.Parse(listBox1Input.Items[i].ToString());
+
+            return nums;
+        }
+
+        //функция вывода
+        private void Output(int count, double avg)
+        {
+            textBox2Avg.Text = avg.ToString("F2");
+            textBox3Count.Text = count.ToString();
+        }
+
+        //функция вызываемая при нажатии на кнопку очистки списка
+        private void button2ClearList_Click(object sender, EventArgs e)
+        {
+            listBox1Input.Items.Clear();
+        }
+
+        //функция вызываемая при нажатии на кнопку добавления элемента в список
+        private void button1Add_Click(object sender, EventArgs e)
+        {
+            listBox1Input.Items.Add(textBox1Num.Text);
+        }
     }
 }
